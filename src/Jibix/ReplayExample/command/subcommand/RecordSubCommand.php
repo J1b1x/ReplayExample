811454284357619884ec53fd52cccdf5a1660179
MEMOxiiii <aalmosleh99@gmail.com>
2025-05-17 05:36:57 +0300

    Update RecordSubCommand.php

--- src/Jibix/ReplayExample/command/subcommand/RecordSubCommand.php
+++ src/Jibix/ReplayExample/command/subcommand/RecordSubCommand.php
@@ -9,28 +9,32 @@ namespace Jibix\ReplayExample\command\subcommand;
 use CortexPE\Commando\args\IntegerArgument;
 use CortexPE\Commando\args\RawStringArgument;
 use CortexPE\Commando\BaseSubCommand;
-use Jibix\Replay\provider\type\MySQLProvider;
-use Jibix\Replay\replay\recorder\RecordHandler;
-use Jibix\Replay\replay\recorder\RecordSettings;
-use Jibix\Replay\util\GameDetails;
+use Jibix\ReplayExample\libs\Jibix\Replay\provider\type\MySQLProvider;
+use Jibix\ReplayExample\libs\Jibix\Replay\replay\recorder\RecordHandler;
+use Jibix\ReplayExample\libs\Jibix\Replay\replay\recorder\RecordSettings;
+use Jibix\ReplayExample\libs\Jibix\Replay\util\GameDetails;
 use Jibix\ReplayExample\Main;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 
-
-class RecordSubCommand extends BaseSubCommand{
+class RecordSubCommand extends BaseSubCommand {
 
     private const DEFAULT_GAME_NAME = "balls";
     private const DEFAULT_ID_LENGTH = 6;
     private const MAX_GAME_NAME_LENGTH = 20;
 
-    protected function prepare(): void{
-        $this->setPermission("replay.command.record");
+    public function __construct(string $name) {
+        parent::__construct($name, "Start recording a replay");
+        $this->setPermission("gamereplay.command.record");
+    }
+
+    protected function prepare(): void {
+        $this->setPermission("gamereplay.command.record");
         $this->registerArgument(0, new RawStringArgument("game-name", true));
         $this->registerArgument(1, new IntegerArgument("identifier-length", true));
     }
 
-    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void{
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
         if (RecordHandler::getInstance()->isRecording($world = $sender->getWorld())) {
             $sender->sendMessage("§cThe world§b {$world->getDisplayName()}§c is already being recorded!");
             return;
@@ -56,5 +60,9 @@ class RecordSubCommand extends BaseSubCommand{
             'xuids' => array_map(fn (Player $player): string => $player->getXuid(), $sender->getWorld()->getPlayers())
         ]));
         $sender->sendMessage("§aThe world§b {$world->getDisplayName()}§a is being recorded now!");
+    }
+
+    public function getPermission(): ?string {
+        return "gamereplay.command.record";
     }
 }
